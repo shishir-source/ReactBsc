0d65813566b1561f7ba7b9ee546236a2c1158ecc
shishir <shishir@bookhint.com>
2018-08-18 19:13:19 +0600

    :initial commit

---
+++ src/app/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+export class Header extends React.Component {
+  render() {
+    return(
+      <nav className="navbar navbar-default">
+          <div className="container">
+              <ul className="nav navbar-nav">
+                  <li><a href="#">Home</a></li>
+              </ul>
+          </div>
+      </nav>
+    );
+  }
+}
---
+++ src/app/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+export class Home extends React.Component {
+  render() {
+      return(
+        <div className="container">
+            <div className="row">
+                <div className="col-xs-12">
+                    <p>P</p>
+                </div>
+            </div>
+        </div>
+      );
+  }
+}
